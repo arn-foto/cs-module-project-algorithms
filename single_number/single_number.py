4ba52d6c796f08407a01e260b1c6c71f6401b392
Anthony N <foto.arn@gmail.com>
2020-09-16 20:26:45 -0700

    initial commit

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,11 +1,36 @@
-'''
-Input: a List of integers where every int except one shows up twice
-Returns: an integer
-'''
-def single_number(arr):
-    # Your code here
+# '''
+# Input: a List of integers where every int except one shows up twice
+# Returns: an integer
+# '''
 
-    pass
+# # Single Number
+
+# Given a non-empty array of integers where every element appears twice except for one. Find that single number. You may assume that there will _always_ be _one_ odd-number-out and every other number in the input shows up exactly twice.  
+
+# ## Examples
+# ```
+# Sample input: [2, 2, 1]
+# Expected output: 1
+# ```
+
+# ```
+# Sample iput: [4, 1, 2, 1, 2]
+# Expected output: 4
+# ```
+
+# Can you implement a solution that finds the single number in _one_ pass through the input array with `O(1)` space complexity?
+
+# ## Testing
+# Run the test file by executing `python test_single_number.py`. 
+
+def single_number(arr):
+    
+    n = []
+    for x in arr:
+        n.append(arr.count(x))
+    for i in range(len(n)):
+        if n[i] == 1:
+            return arr[i]
 
 
 if __name__ == '__main__':
